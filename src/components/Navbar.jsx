c9c03075f12ed88244116599357d0454a243ebbf
Ejekanonu Benjamin <benejekanonu@gmail.com>
2020-07-13 23:08:10 +0100

    Corrected the Mobile Navbar Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,49 +48,48 @@ function Navbar() {
       </section>
 
       {/* mobile Navigation Section */}
-      <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top d-block d-sm-none
-"
-      >
-        <Link className="navbar-brand" to="/">
-          Readers Club
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link " to="/">
-                Home <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/readersclub">
-                Reading Clubs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <section className="d-block d-sm-none">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <Link className="navbar-brand" to="/">
+            Readers Club
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse mx-left" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link " to="/">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/readersclub">
+                  Reading Clubs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </section>
     </>
   );
 }
