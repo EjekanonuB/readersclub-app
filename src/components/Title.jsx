efe2a4be860577688bd8e276d5a25199997ba614
Ejekanonu Benjamin <benejekanonu@gmail.com>
2020-07-16 07:44:54 +0100

    Updated the Document title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import Container from "./Container";
 
 function Title(props) {
   useEffect(() => {
-    document.title = `${props.title} | ReadersApp`;
+    document.title = `${props.title} | Readers Club App`;
     window.scrollTo(0, 0);
   }, [props.title]);
 
